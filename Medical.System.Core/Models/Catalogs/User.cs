edd0092bbbf8ee443a8e7ef995136b96e437a955
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-08-06 15:59:49 -0700

    added mongo decorators to user model

--- Medical.System.Core/Models/Catalogs/User.cs
+++ Medical.System.Core/Models/Catalogs/User.cs
@@ -1,9 +1,15 @@
-﻿namespace Medical.System.Core.Models.Catalogs;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Medical.System.Core.Models.Catalogs;
 
 public class User
 {
-    public int Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-}
+}   
