6d9372b0323d83119ce1f9e3034489dfbbc9f467
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-08-06 20:02:35 -0700

    modified create user model

--- Medical.System.Core/Models/Catalogs/User.cs
+++ Medical.System.Core/Models/Catalogs/User.cs
@@ -9,7 +9,6 @@ public class User
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string UserName { get; set; }
     public string Password { get; set; }
 }   
