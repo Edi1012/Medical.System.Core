593b7eaa409aa67a58b3153160492cc2c25a09fd
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-08-11 21:55:57 -0700

    refactor DTOS(Paths, names)

--- Medical.System.Core/Models/DTOs/CreateUserDto.cs
+++ Medical.System.Core/Models/DTOs/CreateUserDto.cs
@@ -1,9 +1,26 @@
-﻿namespace Medical.System.Core.Models.DTOs;
+﻿using Medical.System.Core.Enums;
+
+namespace Medical.System.Core.Models.DTOs;
 
 public class CreateUserDto
 {
-    public string UserName { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public DateTime BirthDate { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public LoginDTO Login { get; set; }
+    public List<RoleDTO> Roles { get; set; }
+    public AddressDTO Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public Gender Gender { get; set; }
+    public EmergencyContactDTO EmergencyContact { get; set; }
+    public string ProfilePhotoUrl { get; set; }
+    public string Nationality { get; set; }
+    public MaritalStatus MaritalStatus { get; set; }
+    public string OfficialIdNumber { get; set; }
 }
+
+
 
---
+++ Medical.System.Core/Models/DTOs/RoleDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Medical.System.Core.Models.DTOs;
+
+public class RoleDTO
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+
