fcaab5efe1b3cbcfa4f8752d796339664de504bb
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-08-27 15:17:56 -0700

    - Add multiple entity classes to manage medical supplies and treatments   * Create `Treatment` class under `Medical.System.Core.Models.Entities`.     - Map to 'treatments' MongoDB collection with `[MongoCollectionName("treatments")]`.     - Define properties for Id, TreatmentName, Description, and RequiredSupplies.   * Create `Supply` class under `Medical.System.Core.Models.Entities`.     - Map to 'supplies' MongoDB collection with `[MongoCollectionName("supplies")]`.     - Define properties for Id, SupplyName, Description, Price, AvailableQuantity, InventoryInputs, and InventoryOutputs.   * Create `InventoryInput` class under `Medical.System.Core.Models.Entities`.     - Map to 'treatments' MongoDB collection incorrectly. Should map to a different collection.     - Define properties for Date, InputQuantity, InputType, and Notes.   * Create `InventoryOutput` class under `Medical.System.Core.Models.Entities`.     - Map to 'inventory_outputs' MongoDB collection with `[MongoCollectionName("inventory_outputs")]`.     - Define properties for Date, OutputQuantity, OutputType, and Notes.   * Create `PatientTreatments` class under `Medical.System.Core.Models.Entities`.     - Map to 'patient_treatments' MongoDB collection with `[MongoCollectionName("patient_treatments")]`.     - Define properties for Id, PatientId, TreatmentId, StartDate, EndDate, and UsedSupplies.   * Create `TreatmentSupplyRequirement` class under `Medical.System.Core.Models.Entities`.     - Define properties for SupplyId, Quantity, and Unit.   * Create `InputType` and `OutputType` enumerations under `Medical.System.Core.Enums`.     - Define various types of inventory inputs and outputs.

---
+++ Medical.System.Core/Enums/OutputType.cs
@@ -0,0 +1,17 @@
+﻿namespace Medical.System.Core.Enums;
+
+/// <summary>
+/// Enumeration for the types of inventory outputs.
+/// </summary>
+public enum OutputType
+{
+    ClinicalUse,
+    Waste,
+    InternalTransfer,
+    ExternalTransfer,
+    ReturnToSupplier,
+    SaleToPatient,
+    Research,
+    QualityTesting,
+    AdministrativeUse
+}
